17f097d73ba0ec34f1ab5b8d2da7b2ce263ba481
AdamChen <mnre2003@yahoo.com.tw>
2014-11-12 16:02:50 +0800

    init version

---
+++ src/com/adam/app/simplelauncher/AppDetail.java
@@ -0,0 +1,10 @@
+package com.adam.app.simplelauncher;
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+
+	CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
